e4af3fbf297621deaad983928ec35d0263805b48
Bill Levesque <amazingbill2017@gmail.com>
2019-07-01 23:10:23 +0800

    1.

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -3385,12 +3385,12 @@ leave:
       proof_of_work = get_block_longhash(bl, m_db->height());
 
     // validate proof_of_work versus difficulty target
-    if(!check_hash(proof_of_work, current_diffic))
-    {
-      MERROR_VER("Block with id: " << id << std::endl << "does not have enough proof of work: " << proof_of_work << std::endl << "unexpected difficulty: " << current_diffic);
-      bvc.m_verifivation_failed = true;
-      goto leave;
-    }
+//    if(!check_hash(proof_of_work, current_diffic))
+//    {
+//      MERROR_VER("Block with id: " << id << std::endl << "does not have enough proof of work: " << proof_of_work << std::endl << "unexpected difficulty: " << current_diffic);
+//      bvc.m_verifivation_failed = true;
+//      goto leave;
+//    }
   }
 
   // If we're at a checkpoint, ensure that our hardcoded checkpoint hash
