43c8819c743c01039afa875d7ab6a88757f8d006
Bill Levesque <amazingbill2017@gmail.com>
2019-07-27 09:42:10 +0800

    1.change for height 2.recovery crypto folder change

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -109,8 +109,8 @@ static const struct {
   // version 6 starts from block 1400000, which is on or around the 16th of September, 2017. Fork time finalised on 2017-08-18.
   { 6, 1400000, 0, 1503046577 },
 
-  // version 0xa7 starts from block 1882000 , timestamp after block 1882000 + 6000 block
-	{ 0xa7, 1888000, 0, 1562578279 + 6000*120 },
+  // version 0xa7 starts from block 1894000 , timestamp after block 1882000 + 6000 block
+	{ 0xa7, 1894000, 0, 1564191407 },
 };
 static const uint64_t mainnet_hard_fork_version_1_till = 1009826;
 static const struct {
@@ -132,8 +132,8 @@ static const struct {
 
   { 6, 971400, 0, 1501709789 },
 
-		// version 0xa7 starts from block 1881040 , timestamp after block 1876000 + 1week
-	{ 0xa7, 1888000, 0, 1562578279 + 7*24*60*60 },
+		// version 0xa7 starts from block 1894000 , timestamp after block 1876000 + 1week
+	{ 0xa7, 1894000, 0, 1564191407 },
 };
 static const uint64_t testnet_hard_fork_version_1_till = 624633;
 static const struct {
